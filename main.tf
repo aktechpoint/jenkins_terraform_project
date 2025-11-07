2511c35ca524bb4d97271651e9f9bf88d6b5ec23
Abhishek singh <aktechpoint62@gmail.com>
2025-11-07 22:35:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "ap-south-2"
+    region = "ap-south-1"
   
 }
 resource "aws_instance" "abhi_terra" {
     ami = "ami-02d26659fd82cf299"
     instance_type = "t3.micro"
     tags = {
-      Name = "test"
+      Name = "testingssss"
     }
   
 }
